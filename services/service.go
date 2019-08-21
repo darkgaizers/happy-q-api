068771934cd21cca4db935fc465cd14de089e1cf
darkgaizers <darkgaizers@gmail.com>
2019-08-22 02:00:43 +0700

    service

--- services/service.go
+++ services/service.go
@@ -7,16 +7,6 @@ import (
 	"happy-q-api/models"
 )
 
-// StringService provides operations on strings.
-
-/* type QueueServiceInterface interface {
-	Push(*models.Service,*models.Person) (*models.QueueResult, error)
-	Pop() *models.Person
-	View() (*models.QueueView, error)
-} */
-/* Get(ID string) (*models.Person,error)
-Add(*models.Person) error
-Update(*models.PersonUpdate) error */
 type personService struct {
 	repository interfaces.PersonRepository
 }
@@ -27,12 +17,6 @@ func (p *personService) Get(ID string) (*models.Person, error) {
 		return nil, err
 	}
 	return res, nil
-
-	/* 	return &models.Person{
-		ID:   "1",
-		Name: "Test",
-		Type: "MockupType",
-	}, errors.New("not implemented") */
 }
 
 func (p *personService) Add(*models.Person) error {
@@ -44,7 +28,3 @@ func (p *personService) Update(*models.PersonUpdate) error {
 func NewPersonService(er interfaces.PersonRepository) interfaces.PersonServiceInterface {
 	return &personService{er}
 }
-
-// ErrEmpty is returned when an input string is empty.
-/* var popError = errors.New("pop error")
-var pushError = errors.New("push error") */
